52e56986c1760bc0a5aac31b35655c4d20380a8d
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-10-31 03:42:45 +0600

    apis protected with JWT

--- index.js
+++ index.js
@@ -3,12 +3,43 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 const app = express();
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 // middleware
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      // "http://localhost:5173",
+      "https://drivy-auth.web.app",
+      "https://drivy-auth.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
+// jwt token verifier middleware
+const verifier = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    res.status(401).send({ message: "unauthorized access" });
+    return;
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    } else {
+      req.user = decoded;
+      console.log(decoded);
+      next();
+    }
+  });
+
+  console.log("token in mdidleware", token);
+  // next();
+};
 // initial endpoints
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -36,6 +67,21 @@ async function run() {
     const brandCollection = client.db("drivyDB").collection("brands");
     const carCollection = client.db("drivyDB").collection("cars");
     const cartCollection = client.db("drivyDB").collection("cart");
+    // auth related api
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    // data related api
     //post brand data
     app.post("/brands", async (req, res) => {
       const data = req.body;
@@ -112,8 +158,11 @@ async function run() {
       res.send(result);
     });
     //get  cart data by email
-    app.get("/carts/:email", async (req, res) => {
+    app.get("/carts/:email", verifier, async (req, res) => {
       const email = req.params.email;
+      if (req.user.email !== email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
